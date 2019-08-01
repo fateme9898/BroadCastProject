44c66dcdb02399bd59b07e92bb4a3a9f5edb2c9b
fateme9898 <fateme.ad97@gmail.com>
2019-08-02 00:09:55 +0430

    broadcast

---
+++ app/src/main/java/retrofit/anew/broadcastproject/alidost.java
@@ -0,0 +1,13 @@
+package retrofit.anew.broadcastproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class alidost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alidost);
+    }
+}
---
+++ app/src/main/java/retrofit/anew/broadcastproject/davari.java
@@ -0,0 +1,13 @@
+package retrofit.anew.broadcastproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class davari extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_davari);
+    }
+}
